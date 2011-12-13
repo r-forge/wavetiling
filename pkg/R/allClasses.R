f61a2432a9bab0100e62ae36984072c99703fec1
kdbeuf (Kristof De Beuf) <kristof.debeuf@ugent.be>
2011-12-13 12:18:35 +0000

    WaveTilingFeatureSet
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/wavetiling@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/allClasses.R
+++ pkg/R/allClasses.R
@@ -7,6 +7,6 @@ setClass("genomeInfo",representation(chromosome="vector",strand="character",minP
 #Wfm
 setClass("Wfm",representation(betaMAP="matrix",varbetaMAP="matrix",smoothPar="matrix",varEps="numeric",dataOrigSpace="matrix",dataWaveletSpace="matrix",design="matrix",phenoData="data.frame",method="character",genome.info="genomeInfo",n.levels="numeric",probePosition="vector",wave.filt="character",Kj="numeric",prior="character",alpha="numeric",delta="numeric",two.sided="numeric",rescale="matrix",sigProbes="list",regions="list",GlocRegions="list",FDR="matrix",CI="array",F="matrix",varF="matrix",eff="matrix",varEff="matrix"))
 
-#waveTilingFeatureSet
-setClass("waveTilingFeatureSet",contains="TilingFeatureSet")
+#WaveTilingFeatureSet
+setClass("WaveTilingFeatureSet",contains="TilingFeatureSet")
 
