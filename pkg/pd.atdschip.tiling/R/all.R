98f78191f8152015c6b7fb1f15e2a886ed8cbbaa
kdbeuf (Kristof De Beuf) <kristof.debeuf@ugent.be>
2012-05-21 12:46:59 +0000

    added data and annotation packages
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/wavetiling@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: c388d92f92a660608258500d873ae08c5fea166e

---
+++ pkg/pd.atdschip.tiling/R/all.R
@@ -0,0 +1,53 @@
+globals <- new.env(hash=TRUE, parent=emptyenv())
+
+globals$DEBUG <- FALSE
+
+## setup the path at package level so that DB can be accessed
+## during package install/lazyload db creation.
+##
+## We reset the DB_PATH in .onLoad since we need to
+## get the right one based on libpath
+globals$DB_PATH <- system.file("extdata", "pd.atdschip.tiling.sqlite",
+                               package="pd.atdschip.tiling")
+if (nchar(globals$DB_PATH) == 0)
+  stop("Unable to locate DB file")
+
+initDbConnection <- function() {
+    globals$dbCon <- dbConnect(dbDriver("SQLite"), dbname=globals$DB_PATH)
+    globals$dbCon
+}
+
+getDb  <- function() {
+    if (!is.null(globals$dbCon) && isIdCurrent(globals$dbCon))
+      return(globals$dbCon)
+    initDbConnection()
+}
+
+closeDb <- function() {
+    ## FIXME: check for valid connection?
+    sapply(dbListResults(globals$dbCon), dbClearResult)
+    dbDisconnect(globals$dbCon)
+    remove(list="dbCon", envir=globals)
+}
+
+.onLoad <- function(libname, pkgname) {
+    require(SQLite, quietly=TRUE)
+    globals$DB_PATH <- system.file("extdata", "pd.atdschip.tiling.sqlite",
+                                   package="pd.atdschip.tiling",
+                                   lib.loc=libname)
+    if (nchar(globals$DB_PATH) == 0)
+      stop("Unable to locate DB file")
+    ## Establish a connection to the SQLite DB
+    initDbConnection()
+}
+
+.onUnload <- function(libpath) {
+    closeDb()
+}
+
+pd.atdschip.tiling <- new("AffyTilingPDInfo",
+                    genomebuild="TAIR8",
+                    getdb=getDb,
+                    geometry=as.integer(strsplit("2560;2560", ";")[[1]]),
+                    annotation="pd.atdschip.tiling")
+
